48aa7721a969fa23a0c773e9297df2409a782d09
SreekrishnaSridhar <39002090+SreekrishnaSridhar@users.noreply.github.com>
2019-11-23 18:13:28 -0600

    RSA modification

--- RSA.py
+++ RSA.py
@@ -1,9 +1,10 @@
 from Crypto.PublicKey import RSA
 private_key = RSA.generate(1024)
-public_key = private_key.publickey()
-private_pem = private_key.export_key().decode()
-public_pem = public_key.export_key().decode()
-with open('private_pem.pem','w') as pr:
-    pr.write(private_pem)
-with open('public_pem.pem','w') as pu:
-    pu.write(public_pem)
+
+def publickey():
+    public_key = private_key.publickey()
+    private_pem = private_key.export_key().decode()
+    public_pem = public_key.export_key().decode()
+    with open('public_pem.pem','w') as pu:
+        pu.write(public_pem)
+
